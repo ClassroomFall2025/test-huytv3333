87264503da62b3b3dcb0da8cc0e84aa6299fe3df
huytv3333 <huytv05102780@gmail.com>
2025-09-15 14:32:49 +0700

    lab 2 lan 1

--- Lab1/lab1-bai2.py
+++ Lab1/lab1-bai2.py
@@ -24,4 +24,14 @@ print("Diện tích =", dien_tich)
 # Chạy chương trình và kiểm tra kết quả đầu ra.
 # Sinh viên thực hiện bài làm mình ở đây
 
-
+print("\nBài tập 2: Chu vi và diện tích hình chữ nhật")
+# Nhập chiều dài và chiều rộng của hình chữ nhật
+chieu_dai = float(input("Nhập chiều dài: "))
+chieu_rong = float(input("Nhập chiều rộng: "))
+# Tính chu vi và diện tích
+chu_vi_hcn = 2 * (chieu_dai + chieu_rong)
+dien_tich_hcn = chieu_dai * chieu_rong
+# In kết quả
+print("Hình chữ nhật có chiều dài =", chieu_dai, "và chiều rộng =", chieu_rong)
+print("Chu vi =", chu_vi_hcn)
+print("Diện tích =", dien_tich_hcn)
